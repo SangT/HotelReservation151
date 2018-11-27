03ccd33e980170b720a541e80e0b05d1a03e018e
Sang To <tps.sang@gmail.com>
2018-11-26 23:16:28 -0800

    Added hotel file

--- Hotel.java
+++ Hotel.java
@@ -1,6 +1,16 @@
 public class Hotel {
+    private Room[] room;
+    public static final int LUX_PRICE = 300;
+    public static final int ECON_PRICE = 100;
 
-    public static void main(String[] args) {
-
+    public Hotel() {
+        room = new Room[20];
+        for (int i = 0; i < room.length; i++) {
+            if ( i < 10) {
+                room[i] = new Room(i, LUX_PRICE);
+            } else {
+                room[i] = new Room(i, ECON_PRICE);
+            }
+        }
     }
 }
