a2e4ad9d134cb82689c6727a1163f84520fe622a
SangT <tps.sang@gmail.com>
2018-11-28 12:07:12 -0800

    Modified Duration and Reservation

--- src/general/user/Duration.java
+++ src/general/user/Duration.java
@@ -9,21 +9,22 @@ public class Duration {
     private LocalDate checkIn;
     private LocalDate checkOut;
 
+    /**
+     * Contructor takes in checkIn and checkOut date
+     * @param checkIn
+     * @param checkOut
+     */
     public Duration(LocalDate checkIn, LocalDate checkOut) {
         this.checkIn = checkIn;
         this.checkOut = checkOut;
     }
 
+    // The length of stay cannot be longer than 60 nights
     public boolean isValid() {
         if (checkOut.isEqual(checkIn) || checkOut.isBefore(checkIn)) {
             return false;
         }
-
-        // The length of stay cannot be longer than 60 nights
-        if (checkOut.isAfter(checkIn.plusDays(60))) {
-            return false;
-        }
-        return true;
+        return checkOut.isAfter(checkIn.plusDays(60));
     }
 
 }
--- src/general/user/Reservation.java
+++ src/general/user/Reservation.java
@@ -10,18 +10,15 @@ import general.Room;
 public class Reservation {
     /*
     Each reservation record knows who made this reservation, which room is assigned,
-    and the period of the reservation*/
+    and the period of the reservation
+    */
     private Guest guest;
     private Room room;
     private Duration duration;
-    private double price;
 
     public Reservation(Guest guest, Room room, Duration duration) {
         this.guest = guest;
         this.room = room;
         this.duration = duration;
     }
-
-
-
 }
