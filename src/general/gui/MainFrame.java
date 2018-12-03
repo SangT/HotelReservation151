191974971095673745bee1699f870f8c425cb7d9
SangT <tps.sang@gmail.com>
2018-12-03 09:46:35 -0800

    Refactored frame width and height

--- src/general/gui/MainFrame.java
+++ src/general/gui/MainFrame.java
@@ -3,12 +3,12 @@ package general.gui;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
-    public static final int WIDHT = 600;
-    public static final int HEIGHT = 700;
+    public static final int F_WIDTH = 600;
+    public static final int F_HEIGHT = 700;
 
     public MainFrame() {
         setTitle("Hotel Management System");
-        setSize(WIDTH, HEIGHT);
+        setSize(F_WIDTH, F_HEIGHT);
 
         InitialPanel mainPanel = new InitialPanel();
         add(mainPanel);
