4b7c54ffe19c2ae8aaa158df2d31508be8047978
SangT <tps.sang@gmail.com>
2018-12-08 12:36:14 -0800

    Completed InitialPanel of Guest and Manager

--- src/general/gui/InitialPanel.java
+++ src/general/gui/InitialPanel.java
@@ -1,12 +1,14 @@
 package general.gui;
 
+import general.Hotel;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class InitialPanel extends JPanel
 {
-    public InitialPanel()
+    public InitialPanel(Hotel hotel)
     {
         JButton gBtn = new JButton("Guest");
         JButton mBtn = new JButton("Manager");
@@ -14,14 +16,20 @@ public class InitialPanel extends JPanel
         gBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                InfoOption infoOption = new InfoOption(hotel);
+                gBtn.setVisible(false);
+                mBtn.setVisible(false);
+                add(infoOption);
             }
         });
 
         mBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                LogInView logInView = new LogInView(hotel);
+                gBtn.setVisible(false);
+                mBtn.setVisible(false);
+                add(logInView);
             }
         });
 
